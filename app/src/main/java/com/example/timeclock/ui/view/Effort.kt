be45b51095640cb8991fba55eb5d338a1874a6ca
nargok <nargok@gmail.com>
2024-11-17 14:48:38 +0900

    wip timepicker

--- app/src/main/java/com/example/timeclock/ui/view/Effort.kt
+++ app/src/main/java/com/example/timeclock/ui/view/Effort.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CalendarToday
+import androidx.compose.material.icons.filled.Schedule
 import androidx.compose.material3.Button
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
@@ -16,6 +17,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -25,7 +27,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import java.time.LocalDate
+import java.time.LocalTime
 import java.time.format.DateTimeFormatter
+import androidx.compose.material3.rememberTimePickerState
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.TimePickerDialog
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -40,9 +47,29 @@ fun EffortRegisterScreen() {
             selectedDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
         } ?: ""
     }
+
+    var showStartTimePicker by remember {
+        mutableStateOf(false)
+    }
+    val startTimePickerState = rememberTimePickerState()
+    val formattedStartTime = remember(startTimePickerState.hour, startTimePickerState.minute) {
+        LocalTime.of(startTimePickerState.hour, startTimePickerState.minute)
+            .format(DateTimeFormatter.ofPattern("HH:mm"))
+    }
+
+    var showEndTimePicker by remember {
+        mutableStateOf(false)
+    }
+    val endTimePickerState = rememberTimePickerState()
+    val formattedEndTime = remember(endTimePickerState.hour, endTimePickerState.minute) {
+        LocalTime.of(endTimePickerState.hour, endTimePickerState.minute)
+            .format(DateTimeFormatter.ofPattern("HH:mm"))
+    }
+
+
     var date by remember { mutableStateOf("") }
-    var startHour by remember { mutableStateOf("") }
-    var endHour by remember { mutableStateOf("") }
+    var startTime by remember { mutableStateOf("") }
+    var endTime by remember { mutableStateOf("") }
 
     Column(
         modifier = Modifier
@@ -51,6 +78,7 @@ fun EffortRegisterScreen() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
+        // 日付
         OutlinedTextField(
             value = formattedDate,
             onValueChange = {},
@@ -84,26 +112,45 @@ fun EffortRegisterScreen() {
             }
         }
 
+        // 開始時間
         OutlinedTextField(
-            value = startHour,
-            onValueChange = { startHour = it },
+            value = formattedStartTime,
+            onValueChange = {},
             label = { Text("開始") },
             modifier = Modifier.fillMaxWidth(),
+            readOnly = true,
+            trailingIcon = {
+                IconButton(onClick = {
+                    showStartTimePicker = true
+                }) {
+                    Icon(Icons.Filled.Schedule, contentDescription = "開始時間選択")
+                }
+            }
         )
 
+
         OutlinedTextField(
-            value = endHour,
-            onValueChange = { endHour = it },
+            value = formattedEndTime,
+            onValueChange = {},
             label = { Text("終了") },
             modifier = Modifier.fillMaxWidth(),
+            readOnly = true,
+            trailingIcon = {
+                IconButton(onClick = {
+                    showEndTimePicker = true
+                }) {
+                    Icon(Icons.Filled.Schedule, contentDescription = "終了時間選択")
+                }
+            }
         )
+
 
         Button(onClick = {
             println(
                 """
                 日付: $date
-                開始: $startHour
-                終了: $endHour
+                開始: $startTime
+                終了: $endTime
             """.trimIndent()
             )
         }) {
