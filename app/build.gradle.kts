39ea0d43003c83a2f04fca63e46ad61b844c525a
nargok <nargok@gmail.com>
2025-01-25 10:21:00 +0900

    release v0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.nargok.timeclock"
         minSdk = 29
         targetSdk = 34
-        versionCode = 3
-        versionName = "0.3"
+        versionCode = 4
+        versionName = "0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
